955e05c85e3675faff5d50a652dd1867766b2fbc
Robert Gitei <robert.gitei@yopeso.com>
2022-08-29 15:45:09 +0300

    11. Combining flows declaratively

--- start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -25,7 +25,7 @@ import com.example.android.advancedcoroutines.util.CacheOnSuccess
 import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 
 /**
@@ -48,6 +48,17 @@ class PlantRepository private constructor(
             plantService.customPlantSortOrder()
         }
 
+    private val customSortFlow = plantsListSortOrderCache::getOrAwait.asFlow()
+
+    val plantsFlow: Flow<List<Plant>>
+        get() = plantDao.getPlantsFlow()
+            .combine(customSortFlow) { plants, sortOrder ->
+                plants.applySort(sortOrder)
+            }
+            .flowOn(defaultDispatcher)
+            .conflate()
+
+
     /**
      * Fetch a list of [Plant]s from the database.
      * Returns a LiveData-wrapped List of Plants.
@@ -59,9 +70,6 @@ class PlantRepository private constructor(
                 plantList -> plantList.applySort(customSortOrder)
         })
     }
-
-    val plantsFlow: Flow<List<Plant>>
-        get() = plantDao.getPlantsFlow()
 
     fun getPlantsWithGrowZoneFlow(growZoneNumber: GrowZone): Flow<List<Plant>> {
         return plantDao.getPlantsWithGrowZoneNumberFlow(growZoneNumber.number)
